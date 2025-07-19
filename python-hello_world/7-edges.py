37d7bd0dad8c99238aacbfca78a857ce3aafe8d1
Raed-Alqabas <rayeddd55@gmail.com>
2025-07-19 09:46:47 +0000

    trying to fix error on task 5

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = "Hol"
 word_last_2 = "on"
 middle_word = "olberto"
-print(f"First 3 letters: {word_first_3}") #print the first 3 letters
-print(f"Last 2 letters: {word_last_2}") #print last 2 letters
-print(f"Middle word: {middle_word}") #print middle letters
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word: {middle_word}")
